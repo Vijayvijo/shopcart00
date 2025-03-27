432866200ab12355c35c5fefcd0ab359fcb32102
Vijayvijo <vijaycj003@gmail.com>
2025-03-28 00:19:10 +0530

    Fix: Resolve merge conflict and remove duplicate code in wsgi.py

--- vj_project/wsgi.py
+++ vj_project/wsgi.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 WSGI config for vj_project project.
 
@@ -15,21 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vj_project.settings')
 
 application = get_wsgi_application()
-=======
-"""
-WSGI config for vj_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vj_project.settings')
-
-application = get_wsgi_application()
->>>>>>> e7df23b (second commit)
